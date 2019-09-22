09bc10b060431ee1879edd3f55b6eefd4f62153b
Brad Alexander <brad.alexander913@gmail.com>
2019-09-22 20:43:26 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
 class Triangle
-  # write code here
+
+  attr_accessor :a, :b, :c 
+
+  def initialize(a, b, c)
+    @a = a 
+    @b = b 
+    @c = c 
+  end 
+
+  def kind  
+    if ((a <= 0 || b <= 0 || c <= 0) || (a + b <= c || a + c <= b || c + b <= a))
+      begin
+        raise TriangleError 
+      end
+    else
+      if a == b && b == c 
+        :equilateral
+      elsif a == b || a==c || b==c 
+        :isosceles 
+      else 
+        :scalene
+      end
+    end
+  end
+
+
+class TriangleError < StandardError 
+  def message 
+    "This violates the triangle rules."
+  end
+
+end
 end
